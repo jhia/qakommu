3484ff306d7f445701cb0a3e6c1f3c062face870
Alexis Güere <freealex2.0@gmail.com>
2020-01-18 03:17:34 -0400

    [new]Init routes implementations

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -31,6 +31,8 @@ function cleanAttributes(relations) {
     });
 }
 
+function asd(){}
+
 module.exports = {
     findRelation,
     cleanAttributes,
--- modules/attendee/attendee.router.js
+++ modules/attendee/attendee.router.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
 const attendee_controller = require('./attendee.controller');
-router.all('/:id?', (req, res) => {
-  new attendee_controller(req, res);
+
+router.use((req, res, next) => {
+  //Use this to apply a middleware only to this module
+
 });
+
+router.get('/',(req, res) => {
+  //HTTP get route
+  attendee_controller.getFunc();
+});
+
+router.post('/',(req, res) => {
+  ///HTTP post route
+  attendee_controller.postFunc();
+});
+
+router.put('/',(req, res) => {
+  //HTTP put route
+  attendee_controller.putFunc();
+});
+
+router.delete('/',(req, res) => {
+  //HTTP delete route
+  attendee_controller.deleteFunc();
+});
+
 module.exports = router;
