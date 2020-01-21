b980db6d124cc7d08ac30b99140d820882113e68
linuxcoro <idrogo.edixon@gmail.com>
2020-01-20 20:30:51 -0400

    update base.controller.js

--- helpers/base.controller.js
+++ helpers/base.controller.js
@@ -2,17 +2,24 @@
 
 const _ = require('lodash');
 
+const db = require('../models')
+
 function base(name){
 	//controller constructor
+	this.db = db;	
 	this.moduleName = name;
+	this.model = this.db[this.moduleName]
 }
 
 base.prototype.getFunc = function(){
 	return `GET to ${this.moduleName}`;
 }
 
-base.prototype.postFunc = function(){
-	return `POST to ${this.moduleName}`;
+base.prototype.postFunc = async function(req,res){
+	const id = await this.insert(req.body);
+	if (id) {
+		res.status(200).send(id)
+	}
 }
 
 base.prototype.putFunc = function(){
@@ -21,6 +28,15 @@ base.prototype.putFunc = function(){
 
 base.prototype.deleteFunc = function(){
 	return `GET to ${this.moduleName}`;
+}
+
+base.prototype.insert = async function(data){
+	const fillables = _.keys(data) 	
+ 	const res = await this.db[this.moduleName].create(data, {
+		retuning:true,
+		fields: fillables
+	});
+	return res;
 }
 
 module.exports = base;
