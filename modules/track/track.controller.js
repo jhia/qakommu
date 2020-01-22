507d6b08deaddc3733ac9ff1c9525e5dfe37b0f4
carlos ramones <pnfi.carlosramones@gmail.com>
2020-01-21 23:26:13 -0400

    CRUD tracks complete

---
+++ modules/track/track.controller.js
@@ -0,0 +1,106 @@
+'use strict'
+
+const _ = require('lodash');
+const Base = require('../../helpers/base.controller');
+
+const controller = new Base('track');
+
+/*
+*Extend or overwrite the base functions
+*All the controllers already have implicit the models by:
+*this.db -> All models
+*this.model -> Current module model
+*/
+
+controller.getFunc = async function (req, res) {
+
+	const { id } = req.params;
+	const { limit, offset, order, attributes } = req.body;
+	try {
+		const data = await this.getData({
+			id,
+			limit,
+			offset,
+			attributes,
+			order
+		});
+		res.json({
+			data
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: 'something went wrong',
+			data: {}
+		})
+	}
+
+}
+
+
+controller.postFunc = async function (req, res) {
+
+	const { name, description, active, blocker } = req.body;
+	try {
+		let newdate = await this.insert({
+			name,
+			description,
+			active,
+			module_name: this.moduleName
+		});
+		if (newdate) {
+			return res.status(200).json({
+				message: 'successful action',
+				date: newdate
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: 'something went wrong',
+			date: {}
+		});
+	}
+}
+
+controller.putFunc = async function (req, res) {
+	const { id } = req.params;
+	const { name, description, active } = req.body;
+	try {
+		let result = await this.update(
+			{
+				id
+			},
+			{
+				name,
+				description,
+				active
+			});
+			if(result)
+			{
+				res.status(200).json({
+					message: "successful action"
+				});
+			}
+	} catch (error) {
+		res.status(500).json({
+			message: 'something went wrong',
+			date: {}
+		});
+	}
+}
+
+controller.deleteFunc = async function (req, res) {
+	const { id } = req.params;
+	try {
+		let deleterows = await this.delete({ id });
+		res.json({
+			count: deleterows
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: 'something went wrong'
+		});
+	}
+}
+
+
+module.exports = controller;
