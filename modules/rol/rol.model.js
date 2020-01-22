5da91fb975cf4d226e861b70864d6271855463c2
linuxcoro <idrogo.edixon@gmail.com>
2020-01-22 14:51:35 -0400

    singular rol

---
+++ modules/rol/rol.model.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+    const rol = sequelize.define('rol', {
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: {
+              msg: 'Name exist'
+          }
+      },
+        description: DataTypes.STRING,
+    });
+
+    rol.associate = function(models) {
+       rol.hasOne(models.user_type, {
+        foreignKey: 'id_rol',
+        as: 'user_type'
+      });
+    }
+
+    
+    rol.associate = function(models) {
+      rol.hasMany(models.permission, {
+       foreignKey: 'id_rol',
+       as: 'permission'
+     });
+   }
+
+
+    
+    return rol;
+}
