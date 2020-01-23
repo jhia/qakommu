6893a278b85427fbe05dbb8372094f348fae045f
linuxcoro <idrogo.edixon@gmail.com>
2020-01-23 06:43:39 -0400

    create module login

---
+++ modules/login/login.controller.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const _ = require('lodash');
+const Base = require('../../helpers/base.controller');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const controller = new Base('login');
+
+const db = require('../../models')
+const {user} = db
+controller.postFunc = async function(req,res){
+
+        const email = req.body.email || '';
+        const result = await user.findOne({
+            where: {
+                email,
+                active: true
+            }
+
+        });
+
+        if (!result) {
+            this.res.status(404).json({
+                message: 'User Not Found'
+            })
+        } else {
+            const { password }  = req.body;
+            const hash = await bcrypt.compare(password, result.password);
+        }
+        let User = {
+            email
+        };
+
+        User.token = jwt.sign(
+            User,
+            'secret',
+            {
+                expiresIn: '1y',
+                algorithm: 'HS512',
+            }
+        );            
+        return res.status(200).send({
+            response: User.token
+        });
+
+        
+    
+};
+
+
+
+module.exports = controller;
