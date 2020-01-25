577e65eacc4a17820d48533fb0f148d2cdc70d9d
linuxcoro <idrogo.edixon@gmail.com>
2020-01-25 18:11:09 -0400

    validate resource

--- modules/resource/resource.model.js
+++ modules/resource/resource.model.js
@@ -2,9 +2,16 @@
 
 module.exports = (sequelize, DataTypes) => {
     const resource = sequelize.define('resource', {
-        module_name: DataTypes.STRING,
-        description: DataTypes.STRING,
-      })
+      module_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+            args: true,
+            msg: 'Module exist!'
+        }
+      },
+      description: DataTypes.STRING
+    })
   
       resource.associate = function(models) {
         resource.hasMany(models.permission, {
