1bbc0f726997f91bb04c4a15e314204e5ae23339
linuxcoro <idrogo.edixon@gmail.com>
2020-01-25 18:43:07 -0400

    update time for the associate

--- modules/permission/permission.model.js
+++ modules/permission/permission.model.js
@@ -1,6 +1,21 @@
 'use strict';
+
+const Sequelize = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   const permission = sequelize.define('permission', {
+    id_action: Sequelize.INTEGER,
+    id_rol: Sequelize.INTEGER,
+    id_resource: Sequelize.INTEGER,
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    }
   });
   permission.associate = function(models) {
     // associations can be defined here
