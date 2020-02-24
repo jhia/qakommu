167b65cecfcfd89f416a070968c42ea4b531bc1f
carlos ramones <pnfi.carlosramones@gmail.com>
2020-02-24 17:36:50 -0400

    folders repository

---
+++ modules/object_type/object_type.router.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const router = require('express').Router();
+const object_typeController = require('./object_type.controller');
+
+router.use((req, res, next) => {
+  //Use this to apply a middleware only to this module
+  next();
+});
+
+router.get('/',function(req, res){
+  //HTTP get route
+  object_typeController.getFunc(req,res);
+});
+
+router.post('/',(req, res) => {
+  ///HTTP post route
+  object_typeController.postFunc(req,res);
+});
+
+router.put('/',(req, res) => {
+  //HTTP put route
+  object_typeController.putFunc(req,res);
+});
+
+router.delete('/',(req, res) => {
+  //HTTP delete route
+  object_typeController.deleteFunc(req,res);
+});
+
+module.exports = router;
