6e4d6c01e3dcda7f34b01d08de0f37d253e04b2f
carlos ramones <pnfi.carlosramones@gmail.com>
2020-07-08 21:06:19 -0400

    implementation of invoice ticket code

--- modules/ticket_sale_detail/ticket_sale_detail.controller.js
+++ modules/ticket_sale_detail/ticket_sale_detail.controller.js
@@ -42,10 +42,11 @@ controller.getFunc = async function (req, res) {
 
 controller.postFunc = async function (req, res) {
 
-    const { code_ticket, id_ticket_sale, deactivated } = req.body;
+    const {  id_ticket_sale, deactivated } = req.body;
+    let uuid;
     try {
         let newdate = await this.insert({
-            code_ticket, id_ticket_sale, deactivated
+            uuid, id_ticket_sale, deactivated
         });
         if (newdate) {
             return this.response({
@@ -66,13 +67,13 @@ controller.postFunc = async function (req, res) {
 
 controller.putFunc = async function (req, res) {
     const { id } = req.params;
-    const { code_ticket, id_ticket_sale, deactivated, return_data } = req.body;
+    const { id_ticket_sale, deactivated, return_data } = req.body;
     try {
         let result = await this.update(
             {
                 id,
                 data: {
-                    code_ticket, id_ticket_sale, deactivated
+                    id_ticket_sale, deactivated
                 },
                 return_data
             });
