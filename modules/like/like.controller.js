cc61a17710133d77959f5a427ca12d1600245d25
linuxcoro <idrogo.edixon@gmail.com>
2020-08-11 05:22:48 -0400

    add total count like

--- modules/like/like.controller.js
+++ modules/like/like.controller.js
@@ -6,56 +6,67 @@ const jwt = require('jsonwebtoken');
 
 const controller = new Base('like');
 
-
-
 controller.getFunc = async function (req, res) {
+  const { id } = req.params;
+  const { limit, offset, order, attributes } = req.body;
+  const { sequelize } = this.db
 
-    const { id } = req.params;
-    const { limit, offset, order, attributes } = req.body;
-    try {
-        const data = await this.getData({
-            id,
-            limit,
-            offset,
-            attributes: [ 'id_post','id_user', 'reference' ],
-            order
-        });
+  try {
+    const query_like= `SELECT
+    id_post,
+      id_user,
+      reference,
+      COUNT(reference) AS count_reference
+    FROM LIKES
+    WHERE id_post =:id
+    GROUP BY id_post,reference,id_user
+    HAVING COUNT(reference) > 0`;
 
-        return this.response({
-            res,
-            payload: [data]
-        });
-    } catch (error) {
-        return this.response({
-            res,
-            success: false,
-            statusCode: 500,
-            message: 'something went wrong',
-        });
-    }
+    const query_total_like= `SELECT
+    COUNT(*) as total_likes
+    FROM LIKES WHERE id_post =:id`
+
+
+    const x = await sequelize.query(`${query_like}`, { replacements:{id: id}, type: sequelize.QueryTypes.SELECT });
+    const y = await sequelize.query(`${query_total_like}`, { replacements:{id: id}, type: sequelize.QueryTypes.SELECT });
+    const total_likes = y[0].total_likes
+    const data = []
+
+    x.forEach(element => {
+      data.push({
+	"id_post": element.id_post,
+	"id_user": element.id_user,
+	"reference": element.reference,
+	"count_reference": element.count_reference
+
+      })
+    });        
+
+
+
+    return this.response({
+      res,
+      payload: {
+	total_likes,
+	data
+      }
+
+    });
+
+  } catch (error) {
+    return this.response({
+      res,
+      success: false,
+      statusCode: 500,
+      message: 'something went wrong',
+    });
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 controller.postFunc = async function (req, res) {
   const token = req.headers.authorization.split(" ")[1];
   const decoded = jwt.verify(token, 'secret');
-  const { user } = this.db;
+  const { user, user_type } = this.db;
   const email = decoded.email;
 
   let search_id_user = await user.findOne({
@@ -64,11 +75,18 @@ controller.postFunc = async function (req, res) {
 
   const id_user = search_id_user['id'];
 
+  let search_id_community = await user_type.findOne({
+    where: { id_user }
+  });
+
+  const id_community = search_id_community['id_community'];
+
   const { id_post, reference } = req.body;
   try {
     let newdate = await this.insert({
       id_post,
       id_user,
+      id_community,
       reference
     });
     if (newdate) {
@@ -86,19 +104,6 @@ controller.postFunc = async function (req, res) {
       message: err.message,
     });
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
