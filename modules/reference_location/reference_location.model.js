6e20e4cee92527b86a43c1a984f11a40c37bcc2c
linuxcoro <idrogo.edixon@gmail.com>
2020-09-21 06:07:35 -0400

    relation repository

--- modules/reference_location/reference_location.model.js
+++ modules/reference_location/reference_location.model.js
@@ -2,16 +2,21 @@
 
 module.exports = (sequelize, DataTypes) => {
     const reference_location = sequelize.define('reference_location', {
-        id: {
-			allowNull: false,
-			autoIncrement: true,
-			primaryKey: true,
-			type: DataTypes.INTEGER
-		}
+	id_repository: DataTypes.INTEGER,
+ 	reference: {
+	    type: DataTypes.TEXT,
+	    allowNull: false,
+	    unique: {
+		msg: 'location exist'
+	    },    
+	},
     });
 
     reference_location.associate = function(models){
-    	//To create model associations
+ 	reference_location.belongsTo(models.repository, {
+	    foreignKey: 'id_repository',
+	    as: 'repository'
+	});
     }
 
     return reference_location;
