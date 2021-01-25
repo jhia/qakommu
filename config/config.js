eb64d79d029b2dafc93c3af0a3ca9dd0ba00bb5a
Jhinel Arcaya <jhinelarcaya@gmail.com>
2021-01-25 15:01:56 -0400

    config for production

--- config/config.js
+++ config/config.js
@@ -1,15 +1,25 @@
 module.exports = {
   production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  },
+  test: {
     host: process.env.DB_HOST,
-    posrt: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     username: process.env.DB_USER,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: false
   },
   development: {
     host: process.env.DB_HOST,
-    posrt: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     username: process.env.DB_USER,
