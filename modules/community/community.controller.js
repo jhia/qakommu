8cb04b9566a6d3d6f923b7772839cd007477199c
Jhinel Arcaya <jhinelarcaya@gmail.com>
2021-02-09 23:26:29 -0400

    prettify community controller

--- modules/community/community.controller.js
+++ modules/community/community.controller.js
@@ -10,46 +10,46 @@ const controller = new Base('community');
 const jwt = require('jsonwebtoken');
 const { ResponseError } = require('../../http');
 
-function llama(invitation_code, community_id ,time) {
-    let t;
+function llama(invitation_code, community_id, time) {
+	let t;
 
 
-    if (time){ 
-	t = time+"d";
-    }else{ 
-	t = "1d";
-    };
-    let tk = jwt.sign({
-	data: { invitation_code, community_id}
-    }, 'secret', { expiresIn: t });    
-    return tk;
+	if (time) {
+		t = time + "d";
+	} else {
+		t = "1d";
+	};
+	let tk = jwt.sign({
+		data: { invitation_code, community_id }
+	}, 'secret', { expiresIn: t });
+	return tk;
 }
 
 
 controller.getMyCommunities = async function (req, res) {
-		const { limit, offset } = req.query;
-    try {
-			let userCommunities = await this.db.userCommunities.find({
-				where: {
-					userId: req.user.id,
-					active: true
-				},
-				limit,
-				offset,
-				include: [this.db.userCommunity.associations.community],
-				attributes: ['userId', 'owner', 'communityId', 'community'],
-			})
+	const { limit, offset } = req.query;
+	try {
+		let userCommunities = await this.db.userCommunities.find({
+			where: {
+				userId: req.user.id,
+				active: true
+			},
+			limit,
+			offset,
+			include: [this.db.userCommunity.associations.community],
+			attributes: ['userId', 'owner', 'communityId', 'community'],
+		})
 
-			return res.send(userCommunities)
-		} catch ({ message }) {
-			console.log(message)
-			const connectionError = new ResponseError(503, 'Try again later')
-			return res.send(connectionError)
-    }
+		return res.send(userCommunities)
+	} catch ({ message }) {
+		console.log(message)
+		const connectionError = new ResponseError(503, 'Try again later')
+		return res.send(connectionError)
+	}
 }
 
-controller.getPublicCommunities = async function(req, res) {
-//	const q = req.query.q || '';  TODO: search option
+controller.getPublicCommunities = async function (req, res) {
+	//	const q = req.query.q || '';  TODO: search option
 	const { limit, offset } = req.query;
 
 	try {
@@ -74,7 +74,7 @@ controller.getOne = async function (req, res) {
 
 	try {
 		const community = await this.model.findByCode(id)
-		if(!community) {
+		if (!community) {
 			const notFoundError = new ResponseError(404, 'Community does not exist')
 			return res.send(notFoundError)
 		}
@@ -84,7 +84,7 @@ controller.getOne = async function (req, res) {
 		const connectionError = new ResponseError(503, 'Try again later')
 		return res.send(connectionError)
 	}
-	
+
 }
 
 controller.postFunc = async function (req, res) {
@@ -106,40 +106,40 @@ controller.postFunc = async function (req, res) {
 
 	// name
 	try {
-		if(!this.model.validateName(name)) {
+		if (!this.model.validateName(name)) {
 			throw new Error('Name is not valid')
 		}
-	} catch ({message}) {
+	} catch ({ message }) {
 		validationError.addContext('name', message)
 	}
 
 	// description
 	try {
-		if(!this.model.validateDescription(description)) {
+		if (!this.model.validateDescription(description)) {
 			throw new Error('Description is not valid')
 		}
-	} catch ({message}) {
+	} catch ({ message }) {
 		validationError.addContext('description', message)
 	}
 
 	// prefix
 	try {
-		if(!this.model.validatePrefix(prefix)) {
+		if (!this.model.validatePrefix(prefix)) {
 			throw new Error('This subdomain is not valid')
 		}
-	} catch ({message}) {
+	} catch ({ message }) {
 		validationError.addContext('prefix', message)
 	}
 
-	if(req.body.hasOwnProperty('isPrivate')) {
+	if (req.body.hasOwnProperty('isPrivate')) {
 		communityData.isPrivate = !!isPrivate;
 	}
 
-	if(req.body.hasOwnProperty('memberVerification')) {
+	if (req.body.hasOwnProperty('memberVerification')) {
 		communityData.memberVerification = !!memberVerification;
 	}
 
-	if(validationError.hasContext()) {
+	if (validationError.hasContext()) {
 		return res.send(validationError)
 	}
 
@@ -168,71 +168,68 @@ controller.postFunc = async function (req, res) {
 }
 
 controller.putFunc = async function (req, res) {
-		const { id } = req.params;
-		
-		const {
-			name, // required
-			description, // required
-			prefix, // required
-			isPrivate, // default false
-			memberVerification // default false
-		} = req.body;
-		let communityData = {};
+	const { id } = req.params;
 
-		const { name, description, id_type_of_account, users_count, id_website, prefix, member_verification, id_repository, code, is_private, return_data } = req.body;
-		
-		
+	const {
+		name, // required
+		description, // required
+		prefix, // required
+		isPrivate, // default false
+		memberVerification // default false
+	} = req.body;
+	let communityData = {};
+
 	// name
-	if(req.body.name) {
+	if (req.body.name) {
 		try {
-			if(!this.model.validateName(req.body.name)) {
+			if (!this.model.validateName(req.body.name)) {
 				throw new Error('Name is not valid')
 			}
 			communityData.name = req.body.name
-		} catch ({message}) {
+		} catch ({ message }) {
 			validationError.addContext('name', message)
 		}
 	}
 
 	// description
-	if(req.body.description) {
+	if (req.body.description) {
 		try {
-			if(!this.model.validateDescription(req.body.description)) {
+			if (!this.model.validateDescription(req.body.description)) {
 				throw new Error('Description is not valid')
 			}
 			communityData.description = req.body.description;
-		} catch ({message}) {
+		} catch ({ message }) {
 			validationError.addContext('description', message)
 		}
 	}
 
 	// prefix
-	if(req.body.prefix) {
+	if (req.body.prefix) {
 		try {
-			if(!this.model.validatePrefix(req.body.prefix)) {
+			if (!this.model.validatePrefix(req.body.prefix)) {
 				throw new Error('This subdomain is not valid')
 			}
 			communityData.prefix = req.body.prefix;
-		} catch ({message}) {
+		} catch ({ message }) {
 			validationError.addContext('prefix', message)
 		}
 	}
 
-	if(req.body.hasOwnProperty('isPrivate')) {
+	if (req.body.hasOwnProperty('isPrivate')) {
 		communityData.isPrivate = !!isPrivate;
 	}
 
-	if(req.body.hasOwnProperty('memberVerification')) {
+	if (req.body.hasOwnProperty('memberVerification')) {
 		communityData.memberVerification = !!memberVerification;
 	}
 
-	if(validationError.hasContext()) {
+	if (validationError.hasContext()) {
 		return res.send(validationError)
 	}
 
 	try {
 		let community = await this.model.findByCode(id);
-		if(!community) {
+		if (!community) {
 			const notFoundError = new ResponseError(404, 'Community does not exists')
 			return res.send(notFoundError)
 		}
@@ -241,7 +238,7 @@ controller.putFunc = async function (req, res) {
 		await community.save();
 
 		return res.send(community)
-	} catch(err) {
+	} catch (err) {
 		console.log(err.message)
 		const connectionError = new ResponseError(503, 'Try again later')
 		return res.send(connectionError)
