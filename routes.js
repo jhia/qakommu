f53cb2704f7c14ec093ba5bbc271dbd5d3f7b2e6
Jhinel Arcaya <jhinelarcaya@gmail.com>
2021-02-20 18:33:17 -0400

    test for Oracle Cloud CD

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = require('express').Router();
 
 fs
-  .readdirSync(__dirname+'/modules')
+  .readdirSync(__dirname + '/modules')
   .forEach( moduleName => {
 		if(!/^_/.test(moduleName)) {
 			const routerDir = path.join(__dirname,"modules",moduleName, moduleName+'.router.js');
